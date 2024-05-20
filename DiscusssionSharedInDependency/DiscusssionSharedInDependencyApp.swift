af66dc2040dc14422c3bcf841c82a2b273f4bf99
atacan <atacandur@icloud.com>
2024-05-20 21:42:30 +0200

    Initial Commit

---
+++ DiscusssionSharedInDependency/DiscusssionSharedInDependencyApp.swift
@@ -0,0 +1,15 @@
+//
+// https://github.com/atacan
+// 20.05.24
+	
+
+import SwiftUI
+
+@main
+struct DiscusssionSharedInDependencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
